0ffe31ee656cae6f65b5607ed897828a25a5952c
Animesh Karnewar <animesh.karnewar@mobiliya.com>
2018-02-27 17:06:37 +0530

    work in progress ...

--- Strings/ac_substring_matching.py
+++ Strings/ac_substring_matching.py
@@ -11,4 +11,19 @@
     for more info, refer the wiki page -> https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm
 """
 
-# TODO implement this algorithm
+
+def create_fsm(pattern_strings):
+    """
+    The function for creating the finite state machine for the given pattern strings
+    :param pattern_strings: the list of pattern strings
+    :return: goto, failure and output structures
+    """
+    # obtain the vocabulary elements
+    vocab_elems = sorted(set(''.join(pattern_strings)))
+
+    # create the empty structures for the goto, failure and output
+    print(vocab_elems)
+
+
+if __name__ == '__main__':
+    create_fsm(["animesh", "karnewar", "is", "here"])
