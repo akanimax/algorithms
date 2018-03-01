2d827500bd57b6e3b5b6c3d9017278a07b43a7a3
Animesh Karnewar <animesh.karnewar@mobiliya.com>
2018-03-01 17:04:05 +0530

    Sorting algorithms setup

---
+++ Sorting/__init__.py
@@ -0,0 +1,9 @@
+""" The module that contains all the algorithms related to sorting
+"""
+
+# code compatibility setup
+from __future__ import print_function
+from __future__ import absolute_import
+
+# Import the public modules of this package
+import Sorting.insertion_sort_advanced_analysis
---
+++ Sorting/insertion_sort_advanced_analysis.py
@@ -0,0 +1,6 @@
+""" This module contains a way to analyze the number of shifts / swaps needed by the insertion sort
+    algorithm in order to sort the data before sorting the data.
+
+    Insertion sort is a very simple algorithm. Please refer to the wiki page for the same for more
+    information
+"""
